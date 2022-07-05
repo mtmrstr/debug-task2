f01c85f3f7c5f42e8d63634f903056657ba4b884
mtmrstr <106470191+mtmrstr@users.noreply.github.com>
2022-07-05 18:19:53 +0000

    {start}6-3

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
           has_many :books, dependent: :destroy
-
+          has_many :favorites, dependent: :destroy
           validates :name, presence: true , length: {in:  2..20}, uniqueness: true
           validates :introduction, length: {maximum: 50}
 
